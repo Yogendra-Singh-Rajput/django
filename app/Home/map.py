425b6e8395b93efc233455d0ec1968ae43acc84f
yogendra singh <yogendrahello@gmail.com>
2025-03-10 16:59:17 +0530

    make it better

--- app/Home/map.py
+++ app/Home/map.py
@@ -1,7 +1,8 @@
-
+domain = 'https://yogendra-singh-rajput-django--8000.prod1a.defang.dev'
 context={
         'context_images':[
-            ["http://localhost:8000/numerology/","Numerology","/static/numerology.jpeg",1],
-            ["http://localhost:8000/cards/","Cards","/static/cards.jpeg",2]
-        ]
+            [f"{domain}/numerology/","Numerology","/static/numerology.jpeg",1],
+            [f"{domain}/cards/","Cards","/static/cards.jpeg",2]
+        ],
+        'domain':f'{domain}'
 }
--- app/Home/views.py
+++ app/Home/views.py
@@ -56,6 +56,7 @@ def search_fn(s):
 def home(req,context=context):
     print('Home View Accessed')
     context.update({'services_dict':{'Numerology':'numerology/','Cards':'cards/'}.items()})
+    print(context)
     return render(req,'index.html',context=context)
 
 def search(req,context=context):
