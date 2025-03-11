726403a3eb27d0b5326b5d9ab6a52b7265b81262
yogendra singh <yogendrahello@gmail.com>
2025-03-11 12:11:20 +0530

    make it better

--- app/Home/map.py
+++ app/Home/map.py
@@ -1,8 +1,8 @@
 domain = 'https://yogendra-singh-rajput-django--8000.prod1a.defang.dev'
 context={
         'context_images':[
-            [f"{domain}/numerology/","Numerology","/static/numerology.jpeg",1],
-            [f"{domain}/cards/","Cards","/static/cards.jpeg",2]
+            [f"{domain}/home/numerology/","Numerology","/static/numerology.jpeg",1],
+            [f"{domain}/home/cards/","Cards","/static/cards.jpeg",2]
         ],
         'domain':f'{domain}'
 }
